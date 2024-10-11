7f5b91b3827cfd5e4b5e906bb69fb885ee035742
SUNTADTAWAN <123621257+SUNTADTAWAN@users.noreply.github.com>
2024-10-11 14:16:11 +0700

    finish but not finish

--- FRA333_HW3_6520_6531.py
+++ FRA333_HW3_6520_6531.py
@@ -5,18 +5,55 @@
 1.Napat_6520
 2.Tadtawan_6531
 '''
+#import library
+from HW3_utils import FKHW3
+import numpy as np
+
 #=============================================<คำตอบข้อ 1>======================================================#
+
 #code here
-def endEffectorJacobianHW3(q:list[float])->list[float]:
-    pass
+def endEffectorJacobianHW3(q: list[float]) -> np.ndarray:
+    R, P, R_e, p_e = FKHW3(q) 
+
+    n = len(q) 
+
+    J = np.zeros((6, n))
+    
+    for i in range(n):
+        p_i = P[:, i] 
+        
+        z_i = R[:, 2, i] 
+        
+        J[:3, i] = np.cross(z_i, (p_e - p_i))  
+        
+        J[3:, i] = z_i  
+
+    J_e_translational = np.dot(R_e.T, J[:3, :])  
+    J_e_rotational = np.dot(R_e.T, J[3:, :])    
+    J_e = np.vstack((J_e_translational, J_e_rotational))
+    return J_e
 #==============================================================================================================#
 #=============================================<คำตอบข้อ 2>======================================================#
 #code here
 def checkSingularityHW3(q:list[float])->bool:
-    pass
+    J = endEffectorJacobianHW3(q)
+    
+    J_reduced = J[:3, :3] 
+    
+    det_J = np.linalg.det(J_reduced)
+    
+    if (abs(det_J) < 0.001):
+        return 1 
+    else:
+        return 0 
+
 #==============================================================================================================#
 #=============================================<คำตอบข้อ 3>======================================================#
 #code here
 def computeEffortHW3(q:list[float], w:list[float])->list[float]:
-    pass
+    J = endEffectorJacobianHW3(q)
+    
+    tau = np.dot(J.T, w)
+    
+    return tau
 #==============================================================================================================#
--- testSctipt.py
+++ testSctipt.py
@@ -1,24 +1,34 @@
 # file สำหรับตรวจคำตอบ
 # ในกรณีที่มีการสร้าง function อื่น ๆ ให้ระบุว่า input-output คืออะไรด้วย
-import roboticstoolbox as rtb
-import matplotlib
-import numpy as np
-from math import pi
-from HW3_utils import FKHW3
-from FRA333_HW3_xx_xx import endEffectorJacobianHW3,checkSingularityHW3,computeEffortHW3
-from spatialmath import SE3
+
 '''
 ชื่อ_รหัส(ex: ธนวัฒน์_6461)
 1.Napat_6520
 2.Tadtawan_6531
 '''
 
+#import library
+import numpy as np
+from math import pi
+import roboticstoolbox as rtb
+import matplotlib
+matplotlib.use('TkAgg')  # บังคับใช้ Tkinter backend
+import matplotlib.pyplot as plt
+
+from HW3_utils import FKHW3
+from FRA333_HW3_6520_6531 import endEffectorJacobianHW3, checkSingularityHW3, computeEffortHW3
+from spatialmath import SE3
+
+#define variable
 d_1 = 0.0892
-a_2 = -0.425
-a_3 = -0.39243
+a_2 = 0.425
+a_3 = 0.39243
 d_4 = 0.109
 d_5 = 0.093
 d_6 = 0.082
+q_initial = [0, 0, 0]
+q_singulality = [0, 0, pi]
+w = np.array([0, 0, 0, 10, 0, 0])
 
 #Find MDH parameter
 robot = rtb.DHRobot([
@@ -30,18 +40,5 @@ robot = rtb.DHRobot([
                     [0, 1, 0, -d_5],
                     [1, 0, 0, d_4],
                     [0, 0, 0, 1]]),
-                    name = "3-DOF Manipulator")
+                    name = "3DOF_Robot")
 
-#===========================================<ตรวจคำตอบข้อ 1>====================================================#
-#code here
-J_e = endEffectorJacobianHW3(q)
-
-#==============================================================================================================#
-#===========================================<ตรวจคำตอบข้อ 2>====================================================#
-#code here
-
-#==============================================================================================================#
-#===========================================<ตรวจคำตอบข้อ 3>====================================================#
-#code here
-
-#==============================================================================================================#
